8fbe112140a016ed41c2e8a77f212c33e8f6d2f8
KrzysztofSuc <krzysieksuchecki@wp.pl>
2020-07-22 22:56:35 +0200

    added a new config-server module configured the connection of other modules with the cofig-server

--- discovery-server/src/main/java/com/creditApp/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/creditApp/DiscoveryServerApplication.java
@@ -2,6 +2,7 @@ package com.creditApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
